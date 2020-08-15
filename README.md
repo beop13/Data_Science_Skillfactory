b66512d1d45f00e0aea31aba8952547373675983
Karen <mkaren.online@gmail.com>
2020-08-15 22:15:05 +0300

    homework module 0. riddle

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Data_Science_Skillfactory
+
+python3.8 ./riddle.py
---
+++ module_0/riddle.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def game_core_v3(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1,101)
+    left_border, right_border = 1, 100
+    predict = (left_border + right_border)//2
+
+    while number != predict:
+        count+=1
+        if number > predict: 
+            left_border = predict + 1
+            predict = (left_border + right_border) // 2
+        elif number < predict: 
+            right_border = predict - 1
+            predict = (left_border + right_border) // 2
+    return(count) # выход из цикла, если угадали
+
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+score_game(game_core_v3)
